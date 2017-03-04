176d8e768a3e7b1f7e44ddd73f6066e82e6971eb
Kathryn Lybarger <github@zemkat.org>
2017-03-03 22:16:02 -0500

    cleanup comments

--- green_simple.ino
+++ green_simple.ino
@@ -1,3 +1,8 @@
+/*
+  green_simple.ino - simple spinning green animation 
+  sketch for Adafruit Neopixel goggles, based on tutorial example
+  Kathryn Lybarger (@zemkat)
+*/
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
  #include <avr/power.h>
@@ -22,15 +27,15 @@ void loop() {
     uint8_t  i;
     uint32_t t;
 
-    pixels.setPixelColor(offset, color);
-    pixels.setPixelColor(offset+1 % 16, color);
-    pixels.setPixelColor(offset+16, color);
-    pixels.setPixelColor(offset+17 % 16, color);
+    pixels.setPixelColor(offset, color); // first eye
+    pixels.setPixelColor(offset+1 % 16, color); // first eye
+    pixels.setPixelColor(offset+16, color); // second eye
+    pixels.setPixelColor(offset+17 % 16, color); // second eye
     pixels.show();
     delay(50);
-    for(i=0; i<32; i++) { 
+    for(i=0; i<32; i++) { // clear all pixels
         pixels.setPixelColor(i, 0);
     }
-    offset++;
-    offset = offset % 16;
+    offset++; // scoot position
+    offset = offset % 16; // keep between 0 and 15
 }
